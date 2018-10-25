e9f73a3b5b95fbb39feb958c43a2ebeca8652a6f
larabuli <LevanArabuli@users.noreply.github.com>
2018-10-25 21:24:34 +0400

    Add at symbol before checking google domain

--- authz/google.hosted-domain.js
+++ authz/google.hosted-domain.js
@@ -1,5 +1,5 @@
 function isAuthorized(decoded, request, callback, unauthorized, internalServerError, config) {
-  if (decoded.sub.endsWith(config.HOSTED_DOMAIN)) {
+  if (decoded.sub.endsWith(`@${config.HOSTED_DOMAIN}`)) {
     callback(null, request);
   } else {
     unauthorized('Unauthorized. User ' + decoded.sub + ' is not permitted.', callback);
